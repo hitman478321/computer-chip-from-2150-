9649c76c438061085f69e1a9411fcfb802456566
hitman478321 <mymailloi705@gmail.com>
2024-09-11 22:39:21 +0100

    Add files via upload

---
+++ instructions.v
@@ -0,0 +1,92 @@
+////instruction for equation of instruction encoding and decoding
+////instruction for equation of displaying graphics, rendering
+////instruction for equation of game engine
+////instruction for equation of contexualtization/AI
+////instruction for equation of ISP/creation of signals and trasmiteting and receivng 
+////instruction for equation of machine/app/software/PCL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////That's it. We can add a lot of instructions. I believe in creating machines and software just by equations rather than real code; that's what I call "no code," just straight-up equations. 
+////There are no errors or other issues.
+////You can create anything with just one thing: a chip for the system. For the instructions, we need a specific chip that can interact with all signal types—optical for holographic displays, 
+////sound signals for displaying sounds, radio signals, etc.
+////Without actually needing a electricity, even the design will be a magnetic chip for the sake of using magnetic repulsion or magnetic storage and energy provision.
+////It all comes down to physics and chemistry, something I'm not familiar with because I just write code, specifically for hardware.
+////You might say about security; the firmware will already contain blocking mechanisms for CP and other banned content using ISP instruction equation.
+////Do not forget, this is just a fictional system, nothing more.
