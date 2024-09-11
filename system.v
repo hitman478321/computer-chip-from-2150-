3d009876877a50ed1d98816b48e6a23b0e207c51
hitman478321 <mymailloi705@gmail.com>
2024-09-11 22:44:17 +0100

    Update system.v

--- system.v
+++ system.v
@@ -1,15 +1,11 @@
 module dynamic_power_calculator #(parameter WIDTH = 32, parameter SCALAR_SIZE = 24) (
-    $system(" wget https://ia601402.us.archive.org/4/items/wince_x86/WinCEBoot_x86.iso ");  //this willbe dawlaoded by the cpu at the same time as an instruction
     input  wire [SCALAR_SIZE-1:0] scalar_field,  // Scalar field that controls i, it, and n
     output reg  [WIDTH-1:0] result,              // Result of the power calculation
-    input   clk,                            // Clock signal
+    input   clk,                                 // Clock signal
     input   reset    
-    output reg [31:0] f_it,   // Output for f(it)
-    output reg [31:0] f_prime_it  // Output for f'(it)
+    output reg [31:0] f_it,                      // Output for f(it)
+    output reg [31:0] f_prime_it                 // Output for f'(it)
 );
-///i for isntruction
-    /// it for iteratiosn adn 
-    /// and n for numbers of iteratiosn and oepration why the (i^it)^n
     reg [2048:0] i;  
     reg [2048:0] it;    
     reg [2048:0] n;     
@@ -26,7 +22,6 @@ module dynamic_power_calculator #(parameter WIDTH = 32, parameter SCALAR_SIZE = 
             power = temp;
         end
     endfunction
-
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
             i <= 0;  // Reset 'i' to 0
@@ -44,11 +39,10 @@ module dynamic_power_calculator #(parameter WIDTH = 32, parameter SCALAR_SIZE = 
             f_prime_it = 0;  // Derivative for n <= 0
             intermediate_result <= i*f_prime_it;
             result <= power(intermediate_result, n);
-            
             i <= i + 1;
-
         end
     end
-
 endmodule
-
+    ///i for isntruction
+    /// it for iteratiosn adn 
+    /// and n for numbers of iteratiosn and oepration why the (i^it)^n
